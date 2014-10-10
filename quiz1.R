3b8782d6f2508b8ed01ac2f3bfd86b85b5524b48
angus878 <angus_urquhart@yahoo.co.uk>
2014-10-10 07:47:38 +0100

    2nd upload

---
+++ quiz1.R
@@ -0,0 +1,3 @@
+fileUrl2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+setInternet2(use = TRUE)
+download.file(fileUrl, destfile = "./GitHub/cleaningdataR/americancommunity.csv")
