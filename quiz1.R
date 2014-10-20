daf3d671615b181a421038f8cae0ac6c5f781395
angus878 <angus_urquhart@yahoo.co.uk>
2014-10-20 07:36:06 +0100

    quiz2 coomit

--- quiz1.R
+++ quiz1.R
@@ -11,13 +11,13 @@ download.file(fileUrl2, destfile = "./GitHub/cleaningdataR/americancommunity.csv
 df <- read.csv("./GitHub/cleaningdataR/americancommunity.csv",header = TRUE,quote = "\"'",dec = ".")
 
 #group by and count
+library(plyr)
 count(df,"VAL")
 
 #select a single field
 df2 <- df[,c("VAL")]
 
 head(df2) #select top records
-
 
 NROW(df2)  #count records
 
@@ -37,30 +37,58 @@ sum(dat$Zip*dat$Ext,na.rm=T)
 
 
 #now dowload xml
-fileUrl4 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+fileUrl4 <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
 
 install.packages("XML")
+library(XML)
 require(XML)
 
-#can't do https for some reason
-xmlfile=xmlParse("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml")
-class(xmlfile)
+doc <- xmlTreeParse(fileUrl4,useInternal=TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+names(rootNode)
+rootNode[[1]]
+rootNode[[1]][[1]]
+rootNode[[1]][[1]][[2]]
 
-xmltop = xmlRoot(xmlfile)
+rootNode[[1]][[1]][[2]]
 
-xmlName(xmltop) #give name of node, PubmedArticleSet
-xmlSize(xmltop) #how many children in node, 1
-xmlName(xmltop[[1]]) #name of root's childre
+zipcodes <- xpathSApply(rootNode,"//zipcode",xmlValue)
+
+#convert it to a dataframe
+df <- data.frame(matrix(unlist(zipcodes), byrow=T))
+filteredzips <-df[df$matrix.unlist.zipcodes...byrow...T. == '21231',]
+NROW(filteredzips)
 
 
-Madhu2012=ldply(xmlToList("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"), data.frame)
 
-head(Madhu2012)
+fileUrl5 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv "
 
-doc = xmlInternalTreeParse("http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml")
+download.file(fileUrl5, destfile = "./GitHub/cleaningdataR/microdata.csv") 
 
-src = xpathApply(doc, "//a[@href]", xmlGetAttr, "href")
-xmlValue(node)
+
+install.packages("data.table")
+library("data.table")
+
+DT <- fread("./GitHub/cleaningdataR/microdata.csv",header = TRUE)
+
+
+mean(DT$pwgtp15,by=DT$SEX) # wrong
+tapply(DT$pwgtp15,DT$SEX,mean)
+rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2] # wrong
+sapply(split(DT$pwgtp15,DT$SEX),mean)
+mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15)
+
+
+ system.time( replicate(1000, DT[,mean(pwgtp15),by=SEX] ) )
+
+
+ system.time( replicate(1000, tapply(DT$pwgtp15,DT$SEX,mean) ) )
+ system.time( replicate(1000, sapply(split(DT$pwgtp15,DT$SEX),mean) ) )
+ system.time( replicate(1000, mean(DT[DT$SEX==1,]$pwgtp15) ) )
+ system.time( replicate(1000, mean(DT[DT$SEX==2,]$pwgtp15) ) )
+
+
 
 
 
